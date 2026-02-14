d335eaf7ec49550f6e91b1ce7190a18c5dcc107d
DUDA MANOJ KUMAR <manojkumarduda25@gmail.com>
2026-02-14 12:16:45 +0530

    Time: 1 ms (94.75%) | Memory: 43.4 MB (70.51%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,3 +1,7 @@
+//The approach used:let assume ans = 1 bcoz for every row the triangle has 1 at starting and at end.
+//use formula to generate row: ans= ans*(row-col+1)/col.
+//Time complexity:Actually O(2n) By avoiding constant i will be O(n);
+//Space Complexity:O(n) to return the array.
 class Solution {
     public List<Integer>generaterow(int row)
     {
